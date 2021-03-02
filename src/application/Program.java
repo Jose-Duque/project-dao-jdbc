459e52d01557ebdb8de06eace8aa77bc44c66f0d
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-03-02 10:28:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,17 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import db.DB;
-import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "José");
 		System.out.println(obj);
+		
+		Seller seller =new Seller(1, "carlos", "cj@gmail", null, 20.0, obj);
+		System.out.println(seller);
 	}
 
 }
